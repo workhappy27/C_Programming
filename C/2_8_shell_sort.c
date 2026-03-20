f9b08423d5fd224499884e47dfae54e9d0ffc90d
workhappy27 <31071350+workhappy27@users.noreply.github.com>
2026-03-20 14:44:31 +0530

    Update 2_8_shell_sort.c

--- C/2_8_shell_sort.c
+++ C/2_8_shell_sort.c
@@ -19,14 +19,20 @@ int gap=0;
         printf("gap: %d ", gap);
         for(i=gap; i<n; i++) {
             key = arr[i];                         //key = arr[3]
-            for(j=i-1; j>=0 && arr[j] > key; j--) {
-                arr[j+1] = arr[j];            //arr[] = arr[]
-                printf("i=%d, ", i);
+//            for(j=i-1; j>=0 && arr[j] > key; j--) {
+              printf("key: %d ", key);
+              for(j=i; (j>=gap && arr[j-gap] > key); j = j-gap) {
+                arr[j] = arr[j-gap];            //arr[] = arr[]
+                printf("i=%d j=%d j-gap: %d ::", i, j, j-gap);
                 for (k=0; k<n; k++)
                     printf("%d ", arr[k]);
                 printf("\n");
             }
-            arr[j+1] = key;                     // arr[] = key
+            arr[j] = key;                     // arr[] = key
+            printf("\n");
+            for (k=0; k<n; k++)
+                printf("%d ", arr[k]);
+            printf("\n");
     }
 
     }
@@ -35,14 +41,33 @@ int gap=0;
         printf("%d ", arr[k]);
     printf("\n");
 }
-
 /* Output:
+The inner for loop condition (j >= gap && arr[j - gap] > key) compares elements
+that are gap apart and shifts them forward by gap.
 n: 6
 Sort array: 7 3 1 44 35 8
-gap: 3 i=4, 7 3 1 44 44 8
-i=5, 7 3 1 35 44 44
-i=5, 7 3 1 35 35 44
-gap: 1 i=1, 7 7 1 8 35 44
-i=2, 3 7 7 8 35 44
-i=2, 3 3 7 8 35 44
+gap: 3 key: 44
+7 3 1 44 35 8
+key: 35
+7 3 1 44 35 8
+key: 8
+7 3 1 44 35 8
+
+gap: 1 key: 3 i=1 j=1 j-gap: 0 ::7 7 1 44 35 8
+3 7 1 44 35 8
+
+key: 1 i=2 j=2 j-gap: 1 ::3 7 7 44 35 8
+i=2 j=1 j-gap: 0 ::3 3 7 44 35 8
+
+1 3 7 44 35 8
+key: 44
+1 3 7 44 35 8
+key: 35 i=4 j=4 j-gap: 3 ::1 3 7 44 44 8
+
+1 3 7 35 44 8
+key: 8 i=5 j=5 j-gap: 4 ::1 3 7 35 44 44
+i=5 j=4 j-gap: 3 ::1 3 7 35 35 44
+
+1 3 7 8 35 44
 Sorted array: 1 3 7 8 35 44
+*/
